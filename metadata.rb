1bb9595ef66c8babd3bf5a7881df2e2a86c370b4
BK Box <bk@theboxes.org>
2012-10-04 16:37:15 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "marius@promethost.com"
 license          "Apache 2.0"
 description      "Installs/Configures drupal"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 recipe           "drupal", "Installs and configures Drupal"
 recipe           "drupal::cron", "Sets up the default drupal cron"
 recipe           "drupal::drush", "Installs drush - a command line shell and scripting interface for Drupal"
