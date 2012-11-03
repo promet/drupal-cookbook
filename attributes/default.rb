8ec67a8ac595fe5b3cf180a035da68356529e7a4
Marius Ducea <marius.ducea@gmail.com>
2012-11-03 11:08:24 -0700

    move hardcoded localhost value to db host attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,6 +23,7 @@ default['drupal']['checksum'] = "17db094aaa78d76ec6a3517171f1d8c158745eee2e19006
 default['drupal']['dir'] = "/var/www/drupal"
 default['drupal']['db']['database'] = "drupal"
 default['drupal']['db']['user'] = "drupal"
+default['drupal']['db']['host'] = "localhost"
 default['drupal']['site']['admin'] = "admin"
 default['drupal']['site']['pass'] = "drupaladmin"
 default['drupal']['site']['name'] = "Drupal7"
