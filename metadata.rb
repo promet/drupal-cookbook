95a8c8c4f8eb8bde2cbfe2f78b234550fdaf4d48
BK Box <bk@theboxes.org>
2013-02-25 16:33:13 -0600

    Version bump and add lsyncd

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ license          "Apache 2.0"
 name             "drupal"
 description      "Installs/Configures drupal"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.2"
+version          "1.2.0"
 recipe           "drupal", "Installs and configures Drupal"
 recipe           "drupal::cron", "Sets up the default drupal cron"
 recipe           "drupal::drush", "Installs drush - a command line shell and scripting interface for Drupal"
 
-%w{ postfix php apache2 mysql openssl firewall cron }.each do |cb|
+%w{ postfix php apache2 mysql openssl firewall cron lsynd }.each do |cb|
   depends cb
 end
 
