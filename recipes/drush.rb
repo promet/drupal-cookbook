959b322e110cfb944eb1cd323dae37802c427785
Christo DeLange <christo.delange@weather.com>
2013-09-18 11:29:30 -0400

    Don't install drush by default

--- recipes/drush.rb
+++ recipes/drush.rb
@@ -17,6 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "drupal::default"
 
 include_recipe %w{php php::module_mysql php::module_gd}
 
